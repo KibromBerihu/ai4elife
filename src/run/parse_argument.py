dc9ad197ec287313288b3802f264d7d3846d4f59
kibrom2b@gmail.com <kibrom2b@gmail.com>
2022-05-28 13:34:37 +0200

    parse argument

--- src/run/parse_argument.py
+++ src/run/parse_argument.py
@@ -22,9 +22,11 @@ def get_parsed_arguments():
                                                                                 'path'
         )
 
-    parser.add_argument('--from_csv', default=False, type=bool,
-                        help='set true if you provide a csv file with training and validation ids'
-                             'otherwise set false.')
+    parser.add_argument(
+        '--from_csv', default=False, type=bool,
+        help='set true if you provide a csv file with training and validation ids'
+             'otherwise set false.'
+        )
 
     parser.add_argument('--output_dir', dest='output_dir', type=pathlib.Path, help='output directory path')
     parser.add_argument(
@@ -33,6 +35,22 @@ def get_parsed_arguments():
     parser.add_argument('--task', dest='task', choices=['train', 'valid'], help='set training or validataion mode')
     args = parser.parse_args()
 
+    return args
+
+
+def get_parsed_arguments_test_case():
+    """ parses the following important inputs arguments.
+
+    Args:
+         --input_dir: path to the raw pet and ground truth (gt)  folders (you can ignore it).
+        --output_dir: path to save the preprocessed and predicted images (you can ignore it).
+    Returns:
+        Returns the path to the input data and output data for easy use casee or testing case.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', dest='input_dir', default=None, type=pathlib.Path, help='path to raw PET images')
+    parser.add_argument('--output_dir', dest='output_dir', default=None, type=pathlib.Path, help='output directory path')
+    args = parser.parse_args()
     return args
 
 

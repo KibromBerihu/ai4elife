cac4fe72280886663e2a8cd6c7d7b963b3618ea5
Kibrom Berihu <kibrom2b@gmail.com>
2022-07-21 16:46:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(name='ai4elife',
       packages=find_namespace_packages(include=["ai4elife", "ai4elife.*"]),
-      version='0.0.1',
+      version='1.0.0',
       description='ai4elife, Data-centric aI framework for tumor segmentation.',
       url="https://github.com/KibromBerihu/ai4elife",
       author="LITO laboratory, institute Curie",
